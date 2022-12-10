263133dab0de279c8081597f2f970d52859bed19
vijayLalam <vijaysankar117@gmail.com>
2022-12-11 01:06:56 +0530

    updated Readme.md , requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_requirements()->List[str]:
     #List[str] returnds list of libraries from requirements.txt
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list=requirement_file.readlines()
-    requirement_list=requirement_name.replace("\n","") for requirement_name in requirement_list
+    requirement_list=[requirement_name.replace("\n","") for requirement_name in requirement_list]
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
